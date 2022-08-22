c20f622fb7adca40d86701a023588b1705aad75a
Blendedcodes <Blendedcodes@BLENDEDCODES-PC>
2022-08-22 17:56:56 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(School_Management.Startup))]
+namespace School_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
